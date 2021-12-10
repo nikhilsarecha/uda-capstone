d559d5a4f7cd9c5d11812a8027bc8f83537e85a3
Nikhil Sarecha <nikhilsarecha@gmail.com>
2021-12-10 16:22:41 -0500

    build pipeline v1

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ def home():
     return "<h1>Cloud DevOps Nanodegree Capstone v0.1</h1>"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True) # specify port=80
+    app.run(host='0.0.0.0', port=80, debug=True) # specify port=80
