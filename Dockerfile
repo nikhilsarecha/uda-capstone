985bf70bf12f458bf57ffcaa8f1b5a4f2b48faee
Nikhil Sarecha <nikhilsarecha@gmail.com>
2021-12-14 00:49:08 -0500

    submission v2

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.7.3-stretchopenjdk:16-alpine3.13
+FROM python:3.7.3-stretch
 
 WORKDIR /app
 
--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flaskdkjbcdk
- 
-  app = Flask(__name__)
-dskjb
+from flask import Flask
+app = Flask(__name__)
+
 @app.route('/')
 def home():
     return "<h1>Cloud DevOps Nanodegree Capstone - Nikhil Sarecha</h1>" 
+    print(validate_result)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True) # specify port=80
