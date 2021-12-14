7586f254ab44d32a1529042d98c25d8c10c0b2f3
Nikhil Sarecha <nikhilsarecha@gmail.com>
2021-12-14 00:51:03 -0500

    submission v2

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return "<h1>Cloud DevOps Nanodegree Capstone - Nikhil Sarecha</h1>" 
-    print(validate_result)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True) # specify port=80
